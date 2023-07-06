3f82116699b1090a07107c3747867a00cc555f9a
vinothrobinson <vinothrobinson03@gmail.com>
2023-07-06 15:30:24 -0400

    Created a working start button, and create a factory for creating players

--- javascript.js
+++ javascript.js
@@ -1,13 +1,50 @@
 const gameBoard = (() => {
-    let gameArray = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]];
+    let gameArray = ["", "", "", "", "", "", "", "", ""];
     const grids = document.getElementsByClassName("grid");
     for (grid of grids) {
         grid.addEventListener("click", function() {
             console.log(grid.innerHTML)
         })
     }
+
     const resetGameBoard = (() => {
-        gameArray = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]];;
+        gameArray = ["", "", "", "", "", "", "", "", ""];
     })
-    return {resetGameBoard}
+
+    const renderBoard = (() => {
+        let boardSymbol = ""
+
+    })
+
+    return {resetGameBoard, renderBoard}
 })();
+
+const createPlayer = ((name, mark) => {
+    return {name, mark}
+})
+
+const playGame = (() => {
+    let playerList = [];
+    let currentPlayer;
+    let gameOver;
+    let board = document.querySelector(".game-board")
+
+    const startGame = () => {
+        players = [
+            createPlayer("Player One", "X"),
+            createPlayer("Player Two", "O")
+        ]
+        currentPlayer = 0;
+        gameOver = false;
+        board.style.display = "grid";
+        console.log("This runs!")
+    }
+
+    return {startGame}
+})();
+
+const startButton = document.querySelector(".start")
+startButton.addEventListener("click", () => {
+    playGame.startGame()
+    startButton.style.display = "none"
+})
